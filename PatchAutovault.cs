a4efebb13ce0f9173c1a7360f8301d0b43786cd1
Archangel <jesse@archangel.wtf>
2024-08-15 16:58:04 +0200

    Formatting

--- PatchAutovault.cs
+++ PatchAutovault.cs
@@ -1,29 +1,29 @@
-﻿using System.Reflection;
-using SPT.Reflection.Patching;
+﻿using Comfort.Common;
 using EFT;
+using SPT.Reflection.Patching;
+using System.Reflection;
 using static GClass960;
-using Comfort.Common;
 
 namespace StopHotkeyAutoVault
 {
-    public class PatchAutovault : ModulePatch
-    {
-        protected override MethodBase GetTargetMethod() => typeof(Player).GetMethod(nameof(Player.Vaulting), BindingFlags.Public | BindingFlags.Instance);
+	public class PatchAutovault : ModulePatch
+	{
+		protected override MethodBase GetTargetMethod() => typeof(Player).GetMethod(nameof(Player.Vaulting), BindingFlags.Public | BindingFlags.Instance);
 
-        [PatchPrefix]
-        private static bool PatchPrefix(ref float ____vaultingTiming, Player __instance)
-        {
+		[PatchPrefix]
+		private static bool PatchPrefix(ref float ____vaultingTiming, Player __instance)
+		{
 			if (Singleton<SharedGameSettingsClass>.Instance.Game.Settings.AutoVaultingMode == EAutoVaultingUseMode.Automatic)
-            {
-                //Run original method, this will leave the player to autovault over everything until the end of the raid if he switches it back mid-raid
-                return true;
-            }
+			{
+				//Run original method, this will leave the player to autovault over everything until the end of the raid if he switches it back mid-raid
+				return true;
+			}
 
-            ____vaultingTiming = 0f;
-            __instance.method_14();
-            
-            //Skip original method
-            return false;
+			____vaultingTiming = 0f;
+			__instance.method_14();
+
+			//Skip original method
+			return false;
 		}
-    }
+	}
 }
--- Plugin.cs
+++ Plugin.cs
@@ -2,12 +2,12 @@
 
 namespace StopHotkeyAutoVault
 {
-    [BepInPlugin("com.archangelwtf.stophotkeyautovault", "Stop Hotkey Auto-Vault", "1.0.0")]
-    public class Plugin : BaseUnityPlugin
-    {
-        public void Awake()
-        {
+	[BepInPlugin("com.archangelwtf.stophotkeyautovault", "Stop Hotkey Auto-Vault", "1.0.0")]
+	public class Plugin : BaseUnityPlugin
+	{
+		public void Awake()
+		{
 			new PatchAutovault().Enable();
 		}
-    }
+	}
 }
